c92c632c6f267c21aad5e72bcd076e27772cce14
Will Lewis <will@wflewis.com>
2018-06-05 15:12:26 -0400

    Required sig

--- src/i.cr
+++ src/i.cr
@@ -1,3 +1,18 @@
 module I
   VERSION = {{ `shards version __DIR__`.chomp.stringify }}
+
+  def script(script_name : String)
+    yield nil, nil, nil
+  end
 end
+
+include I
+
+script "say hello" do |name|
+  puts "hello, #{name}"
+end
+
+# invoke with
+# $ i say hello : will
+# $ i say hello: will
+# $ i say hello :will
